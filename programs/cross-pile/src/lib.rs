fd733b6eb5ca3e4a13e6d5a008358e265834ecb8
user-of-dev1-noflaws-droplet <user-of-dev1-noflaws-droplet@example.org>
2022-01-15 00:07:55 +0000

    bump; changed payer

--- programs/cross-pile/src/lib.rs
+++ programs/cross-pile/src/lib.rs
@@ -59,7 +59,7 @@ pub mod cross_pile {
             authority: ctx.accounts.initiator.to_account_info(),
             new_authority: ctx.accounts.coin.to_account_info(),
             system_program: ctx.accounts.system_program.to_account_info()
-        };
+        };	
 
         let cpi_context = CpiContext::new(
             ctx.accounts.solrand_program.clone(),
@@ -216,7 +216,7 @@ pub struct CreateCoin<'info> {
         init, 
         seeds = [b"coin-seed".as_ref(), initiator.key().as_ref()],
         bump = coin_bump,
-        payer = acceptor,
+        payer = initiator,
         space = 8 + size_of::<Coin>()
     )]
     pub coin: AccountLoader<'info, Coin>,
